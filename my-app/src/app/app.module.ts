5a2c81e54b4ebda3002bb6669350375094a2c4df
rlterry00 <chuckterry00@gmail.com>
2018-06-14 02:56:06 -0400

    testing vs git

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
+import { SearchBoxDirective } from './search-box.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    OrdersComponent,
+    SearchBoxDirective
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/search-box.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[rlSearchBox]'
+})
+export class SearchBoxDirective {
+
+  constructor() { }
+
+}
